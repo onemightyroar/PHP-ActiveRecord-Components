0f0d8fd85b8a2cdf4a18c333770c4d99f997c9f9
Brian Muse <brian@onemightyroar.com>
2013-04-09 20:02:44 -0400

    Adding helper method to convert an array of models to profiles.

--- src/OneMightyRoar/PHP_ActiveRecord_Components/Utils.php
+++ src/OneMightyRoar/PHP_ActiveRecord_Components/Utils.php
@@ -68,4 +68,45 @@ class Utils {
 		return ( $sql_timestamp_string === $php_timestamp_string );
 	}
 
+    /**
+     * Convert objects to an array of profiles
+     *
+     * @access protected
+     * @param array $objects
+     * @return array The objects, converted to profiles using the get_profile method in each model
+     */
+    public static function models_to_profiles( array $objects = null, array $includes = array() ) {
+        $return_array = array();
+
+        if( is_null( $objects ) ) {
+            return $return_array;
+        }
+
+        foreach( $objects as $object ) {
+            if( $object instanceof ModelInterface === false ) {
+                throw new \InvalidArgumentException( get_class( $object ) . ' should implement ' . __NAMESPACE__ . '\ModelInterface' );
+            }
+
+            $object_as_profile = $object->get_profile();
+
+            // Add profiles for models we're including
+            foreach( $includes as $include ) {
+                // Make sure the included object exists
+                if( isset( $object->{$include} ) && !is_null( $object->{$include} ) ) {
+                    $object_to_include = $object->{$include};
+
+                    if( $object_to_include instanceof ModelInterface === false ) {
+                        throw new \InvalidArgumentException(get_class( $object_to_include ) . ' should implement ' . __NAMESPACE__ . '\ModelInterface');
+                    }
+
+                    $object_as_profile[$include] = $object_to_include->get_profile();
+                }
+            }
+
+            $return_array[] = $object_as_profile;
+        }
+
+        return $return_array;
+    }
+
 } // End class Utils
